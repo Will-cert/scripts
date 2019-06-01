ac75c40fb6d60ba096c8cba931375c149881ffeb
zeromorphism <zeromorphism@curried.ninja>
2019-06-01 15:54:42 -0400

    Fixing syntax error and updating bash expansion

--- addpath.sh
+++ addpath.sh
@@ -1,10 +1,10 @@
 
-PATH_TO_ADD=$1
-
-if [[-d $PATH_TO_ADD]]
+read PATH_TO_ADD
+echo $PATH_TO_ADD
+if [[ -d $PATH_TO_ADD ]]
 then 
-    sed "s/export PATH=\"\(.*\)\"/export PATH=\"\1\:$test\"/g" ~/.bashrc
-elif [[ -f $PATH_TO_ADD]]; then
+    sed "s/export PATH=\"\(.*\)\"/export PATH=\"\1\:${PATH_TO_ADD//\//\\/}\"/g" ~/.bashrc
+elif [[ -f $PATH_TO_ADD ]]; then
     echo "$PATH_TO_ADD is a file and not a directory."
     exit 1
 else
